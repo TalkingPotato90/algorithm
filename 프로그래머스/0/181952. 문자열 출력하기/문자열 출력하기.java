5a01cac6c9dfbb28f0b305010c93d709b8a8480a
TalkingPotato90 <138276030+TalkingPotato90@users.noreply.github.com>
2024-04-19 09:48:02 +0900

    [level 0] Title: 문자열 출력하기, Time: 127.69 ms, Memory: 60.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append(br.readLine());
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
