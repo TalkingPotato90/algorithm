96eab00d6d66db81cc80b1b3effb5f07cbf784d2
TalkingPotato90 <138276030+TalkingPotato90@users.noreply.github.com>
2024-05-17 13:59:15 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.48 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -3,8 +3,9 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] numbers) {
         Arrays.sort(numbers);
+        int length = numbers.length;
         
-        int temp1 = numbers[numbers.length - 1] * numbers[numbers.length - 2];
+        int temp1 = numbers[length - 1] * numbers[length - 2];
         int temp2 = temp1;
         
         if(numbers[0] < 0 && numbers[1] < 0){
