5e6830da1f11fa068eb052038d717bcdac863bc8
TalkingPotato90 <138276030+TalkingPotato90@users.noreply.github.com>
2024-09-23 18:01:45 +0900

    [Silver IV] Title: 괄호, Time: 100 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.Stack;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int number = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < number; i++) {
+            sb.append(judge(br.readLine()));
+            sb.append("\n");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    public static String judge(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if (c == '(') {
+                stack.push(c);
+            }else if(c == ')'){
+                if(stack.isEmpty()){
+                    return "NO";
+                }else if(stack.peek() == '('){
+                    stack.pop();
+                }
+            }
+        }
+
+        if(stack.isEmpty()){
+            return "YES";
+        }else {
+            return "NO";
+        }
+    }
+}
