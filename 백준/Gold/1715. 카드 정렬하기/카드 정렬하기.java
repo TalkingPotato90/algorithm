3e110f85ae6c0112f83670b6d42d54cf51750110
TalkingPotato90 <138276030+TalkingPotato90@users.noreply.github.com>
2024-09-27 20:35:49 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 324 ms, Memory: 25376 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.PriorityQueue;
+
+public class Main {
+        public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int lines = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> cards = new PriorityQueue<>();
+
+        for (int i = 0; i < lines; i++) {
+            cards.add(Integer.parseInt(br.readLine()));
+        }
+
+        int result = 0;
+
+        while (cards.size() > 1) {
+            int first = cards.poll();
+            int second = cards.poll();
+
+            int sum = first + second;
+
+            result += sum;
+
+            cards.add(sum);
+        }
+
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+    }
+}
