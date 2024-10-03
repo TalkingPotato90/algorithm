ab94627bfa8cfbedd36dbca0c87cec24690f3e82
TalkingPotato90 <138276030+TalkingPotato90@users.noreply.github.com>
2024-10-03 22:52:04 +0900

    [Bronze V] Title: 사칙연산, Time: 124 ms, Memory: 15860 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String[] numbers = br.readLine().split(" ");
+        
+        int a = Integer.parseInt(numbers[0]);
+        int b = Integer.parseInt(numbers[1]);
+        
+        bw.write(a+b+"\n");
+        bw.write(a-b+"\n");
+        bw.write(a*b+"\n");
+        bw.write(a/b+"\n");
+        bw.write(a%b+"\n");
+        bw.flush();
+        bw.close();
+    }
+}
