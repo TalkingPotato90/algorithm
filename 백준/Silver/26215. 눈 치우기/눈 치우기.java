a62c2e7fe14909ff109012e990a8784b9a6a823e
TalkingPotato90 <138276030+TalkingPotato90@users.noreply.github.com>
2024-10-29 15:51:51 +0900

    [Silver III] Title: 눈 치우기, Time: 112 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/26215. 눈 치우기/눈 치우기.java
+++ 백준/Silver/26215. 눈 치우기/눈 치우기.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -12,22 +10,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        List<Integer> list = new ArrayList<>();
+        
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
 
         for (int i = 0; i < N; i++) {
-            list.add(Integer.parseInt(st.nextToken()));
+            queue.add(Integer.parseInt(st.nextToken()));
         }
-
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
-        queue.addAll(list);
-
+        
         int count = 0;
 
         while (!queue.isEmpty()) {
             int pop1 = queue.poll();
             pop1--;
-
 
             if(!queue.isEmpty()) {
                 int pop2 = queue.poll();
