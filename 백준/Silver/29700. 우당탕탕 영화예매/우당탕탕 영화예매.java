b2035c93ef0835f74bdabc454d2eb9339b258c72
TalkingPotato90 <138276030+TalkingPotato90@users.noreply.github.com>
2024-11-19 12:19:08 +0900

    [Silver IV] Title: 우당탕탕 영화예매, Time: 552 ms, Memory: 295580 KB -BaekjoonHub

--- 백준/Silver/29700. 우당탕탕 영화예매/우당탕탕 영화예매.java
+++ 백준/Silver/29700. 우당탕탕 영화예매/우당탕탕 영화예매.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -11,13 +11,17 @@ public class Main{
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        String empty = "0";
+        String empty = "0".repeat(K);
         int count = 0;
 
         for (int i = 0; i < N; i++) {
             String s = br.readLine();
+            if (!s.contains(empty)) {
+                continue;
+            }
+
             for(int j = 0; j <= M - K; j++) {
-                if(s.substring(j, j + K).equals(empty.repeat(K))) {
+                if(s.substring(j, j + K).equals(empty)) {
                     count++;
                 }
             }
