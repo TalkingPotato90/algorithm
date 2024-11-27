62e84b3664e14753708b292571c498e331c9ee46
TalkingPotato90 <138276030+TalkingPotato90@users.noreply.github.com>
2024-11-27 17:54:02 +0900

    [Gold III] Title: 양 구출 작전, Time: 1252 ms, Memory: 75208 KB -BaekjoonHub

--- 백준/Gold/16437. 양 구출 작전/양 구출 작전.java
+++ 백준/Gold/16437. 양 구출 작전/양 구출 작전.java
@@ -1,15 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int island = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
+        int island = Integer.parseInt(br.readLine());
         List<Integer>[] graph = new ArrayList[island + 1];
         for (int i = 1; i <= island; i++) {
             graph[i] = new ArrayList<>();
@@ -24,21 +21,18 @@ public class Main {
             int connect = Integer.parseInt(st.nextToken());
 
             counts[i] = type.equals("W") ? -count : count;
-
             graph[connect].add(i);
         }
 
-        System.out.println(dfs(1, graph, counts));
-
+        sb.append(dfs(1, graph, counts)).append("\n");
+        System.out.print(sb);
     }
 
     private static long dfs(int node, List<Integer>[] graph, long[] animalCount) {
         long totalSheep = 0;
-
         for (int child : graph[node]) {
             totalSheep += dfs(child, graph, animalCount);
         }
-
         totalSheep += animalCount[node];
         return Math.max(0, totalSheep);
     }
