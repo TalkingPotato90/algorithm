0218be6f55959e95e19d42cc67a706a8a123bac1
TalkingPotato90 <138276030+TalkingPotato90@users.noreply.github.com>
2024-12-17 13:14:39 +0900

    [Silver IV] Title: 바닥 장식, Time: 116 ms, Memory: 14756 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.java
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.java
@@ -2,50 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-    public static int[][] wood;
-    public static boolean[][] visited;
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        
-        wood = new int[n][m];
-        visited = new boolean[n][m];
+
+        int[][] wood = new int[n][m];
+
         int count = 0;
+
+        for (int i = 0; i < n; i++) {
+          String[] input = br.readLine().split("");
+          for (int j = 0; j < m; j++) {
         
-        for(int i = 0; i < n; i++){
-            String[] input = br.readLine().split("");
-            for(int j = 0; j < m; j++){
-                wood[i][j] = input[j].equals("-") ? 1:2;
+            wood[i][j] = input[j].equals("-") ? 1 : 2;
+        
+            if (j == 0 && wood[i][j] == 1) {
+              count++;
+              continue;
             }
-        }
-        
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(!visited[i][j] && wood[i][j] == 1){
-                    visited[i][j] = true;
-                    while(j + 1 < m && wood[i][j+1] == 1){
-                        visited[i][++j] = true;
-                    }
-                    count++;
-                }
+
+            if (j > 0 && wood[i][j] == 1) {
+              if (wood[i][j - 1] == wood[i][j]) {
+                continue;
+              }
             }
-        }
-        
-        for(int j = 0; j< m; j++){
-            for(int i = 0; i < n; i++){
-                if(!visited[i][j] && wood[i][j] == 2){
-                    visited[i][j] = true;
-                    while (i + 1 < n && wood[i+1][j] == 2){
-                        visited[++i][j] =true;
-                    }
-                    count++;
-                }
+
+            if (i == 0 && wood[i][j] == 2) {
+              count++;
+              continue;
             }
+
+            if (i > 0 && wood[i][j] == 2) {
+              if (wood[i - 1][j] == wood[i][j]) {
+                continue;
+              }
+            }
+
+            count++;
+          }
         }
-        
+
         System.out.println(count);
-    }
+      }
 }
