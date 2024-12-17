d507c0c965efc097e8f2f8a71100297390261acb
TalkingPotato90 <138276030+TalkingPotato90@users.noreply.github.com>
2024-12-17 13:35:58 +0900

    [Silver IV] Title: 바닥 장식, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.java
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.java
@@ -2,49 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+      public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
 
-        int[][] wood = new int[n][m];
+    int[][] wood = new int[n + 1][m + 1];
 
-        int count = 0;
+    int count = 0;
 
-        for (int i = 0; i < n; i++) {
-          String[] input = br.readLine().split("");
-          for (int j = 0; j < m; j++) {
-        
-            wood[i][j] = input[j].equals("-") ? 1 : 2;
-        
-            if (j == 0 && wood[i][j] == 1) {
-              count++;
-              continue;
-            }
+    for (int i = 1; i <= n; i++) {
+      String input = br.readLine();
+      for (int j = 1; j <= m; j++) {
+        wood[i][j] = input.charAt(j - 1) == '-' ? 1 : 2;
 
-            if (j > 0 && wood[i][j] == 1) {
-              if (wood[i][j - 1] == wood[i][j]) {
-                continue;
-              }
-            }
-
-            if (i == 0 && wood[i][j] == 2) {
-              count++;
-              continue;
-            }
-
-            if (i > 0 && wood[i][j] == 2) {
-              if (wood[i - 1][j] == wood[i][j]) {
-                continue;
-              }
-            }
-
-            count++;
-          }
+        if ((wood[i][j] == 1 && wood[i][j - 1] == 1) || (wood[i][j] == 2 && wood[i - 1][j] == 2)) {
+          continue;
         }
 
-        System.out.println(count);
+        count++;
       }
+    }
+
+    System.out.println(count);
+  }
 }
