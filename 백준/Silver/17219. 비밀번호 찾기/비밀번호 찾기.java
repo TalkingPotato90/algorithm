8a8e2f46b1763477d9becf14103589cd7bea1dde
TalkingPotato90 <138276030+TalkingPotato90@users.noreply.github.com>
2025-01-08 20:23:06 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 592 ms, Memory: 86712 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -9,21 +9,46 @@ public class Main{
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        Map<String,String> map = new HashMap<>();
+        Map<Integer, List<Entry>> map = new HashMap<>();
+
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            char[] site = st.nextToken().toCharArray();
+            char[] pass = st.nextToken().toCharArray();
+
+            int siteHash = Arrays.hashCode(site);
+
+            map.putIfAbsent(siteHash, new ArrayList<>());
+            map.get(siteHash).add(new Entry(site, pass));
+        }
+
         StringBuilder sb = new StringBuilder();
 
-        for(int i = 0; i < n + m; i++){
-            String input = br.readLine();
-            if( i < n){
-                String[] tokens = input.split(" ");
-                String site = tokens[0].intern(); 
-                String pass = tokens[1].intern();
-                map.put(site, pass);
-            } else {
-                String site = input.intern();
-                sb.append(map.get(site)).append("\n");
+        for(int i = 0; i < m; i++){
+            char[] site = br.readLine().toCharArray();
+            int siteHash = Arrays.hashCode(site);
+
+            List<Entry> entries = map.get(siteHash);
+            if (entries != null) {
+                for (Entry entry : entries) {
+                    if (Arrays.equals(entry.site, site)) {
+                        sb.append(entry.pass).append("\n");
+                        break;
+                    }
+                }
             }
         }
-        System.out.println(sb);
+
+        System.out.print(sb.toString());
+    }
+
+    static class Entry {
+        char[] site;
+        char[] pass;
+
+        Entry(char[] site, char[] pass) {
+            this.site = site;
+            this.pass = pass;
+        }
     }
 }
