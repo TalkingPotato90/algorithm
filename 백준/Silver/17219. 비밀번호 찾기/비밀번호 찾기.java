e1f169fa640671e7f40329147102278c2a81ff8f
TalkingPotato90 <138276030+TalkingPotato90@users.noreply.github.com>
2025-01-08 20:01:00 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 828 ms, Memory: 75888 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -5,24 +5,25 @@ public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        
+
         Map<String,String> map = new HashMap<>();
-        
-        for(int i = 0; i < n; i++){
-            st = new StringTokenizer(br.readLine());
-            String site = st.nextToken();
-            String pass = st.nextToken();
-            
-            map.put(site, pass);
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < n + m; i++){
+            String input = br.readLine();
+            if( i < n){
+                String[] tokens = input.split(" ");
+                String site = tokens[0].intern(); 
+                String pass = tokens[1].intern();
+                map.put(site, pass);
+            } else {
+                String site = input.intern();
+                sb.append(map.get(site)).append("\n");
+            }
         }
-        
-        for(int i =0; i < m; i++){
-            String site = br.readLine();
-            
-            System.out.println(map.get(site));
-        }
+        System.out.println(sb);
     }
 }
