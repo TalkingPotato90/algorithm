35ec8d08233086786e3d01b7d658921df604ec04
TalkingPotato90 <138276030+TalkingPotato90@users.noreply.github.com>
2025-02-18 23:25:15 +0900

    [Gold IV] Title: 문자열 폭발, Time: 364 ms, Memory: 25304 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main{
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String str = br.readLine();
+    String bomb = br.readLine();
+
+    StringBuilder sb = new StringBuilder();
+    for (char c : str.toCharArray()) {
+      sb.append(c);
+
+      if (sb.length() >= bomb.length()) {
+        boolean match = true;
+        for (int i = 0; i < bomb.length(); i++) {
+          if (sb.charAt(sb.length() - bomb.length() + i) != bomb.charAt(i)) {
+            match = false;
+            break;
+          }
+        }
+        if (match) {
+          sb.setLength(sb.length() - bomb.length());
+        }
+      }
+    }
+
+    System.out.println(sb.length() == 0 ? "FRULA" : sb.toString());
+  }
+}
